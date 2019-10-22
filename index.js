65dfe78b1a73afba513d8b7495ebe6ef390a54fc
gladimir jacobsen <gladimirjacobsen@gmail.com>
2019-10-22 18:02:34 -0300

    Define basic routes

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// Database connection
 connectDB();
 
 app.get('/', (req, res) => res.send('API runing'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
