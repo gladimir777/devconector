2e99379f0bd43e8e42693817d70a6f40b825a04b
gladimir jacobsen <gladimirjacobsen@gmail.com>
2019-10-22 19:42:47 -0300

    Set user validation

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API runing'));
 
+// Initialize midleware
+app.use(express.json({ extended: false }));
+
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
-// @route GET api/user
-// @desc  test route
+// @route POST api/user
+// @desc  register user
 // @acces public
-router.get('/', (req, res) => {
-  res.send('User route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please provide a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
